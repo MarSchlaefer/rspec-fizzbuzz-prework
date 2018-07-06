0dfc8bef546796f800810d506f98b164bf7a88d9
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-07-06 20:34:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
   if num % 3 == 0 
     return "Fizz"
-  if num % 5 == 0 
+  elsif num % 5 == 0 
     return "Buzz"
-  if (num % 3 == 0) && (num % 5 == 0)
+  elsif (num % 3 == 0) && (num % 5 == 0)
     return "FizzBizz"
   end
 end
